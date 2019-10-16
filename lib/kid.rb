1c1fcb9164650432c377de634917d3b002e7abf6
RubenV-Dev <r.will16@hotmail.com>
2019-10-16 16:43:20 -0400

    1

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-    # include Dance 
-    extend MetaDancing
+    include Dance
 
-    # attr_accessor :name
+    # extend MetaDancing
 
-    # def initialize(name)
-    #     @name = name
-    # end
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
 end
